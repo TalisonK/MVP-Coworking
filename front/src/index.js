9df8dbb65496f7833089338d78d8ac2dfebb8b30
kenin <talisonkennedy1@gmail.com>
2020-09-15 02:44:42 -0300

    inicio da implementação do status de admin

--- front/src/index.js
+++ front/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import App from "./App";
 
-localStorage.setItem("url", 'http://142.93.183.229:3030');
+//localStorage.setItem("url", 'http://142.93.183.229:3030');
+localStorage.setItem("url", 'http://localhost:3030');
 
 ReactDOM.render(<App/>,document.getElementById('root'));
